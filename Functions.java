66da0293d988d0e31c501a0ef0b6fc4aca340d9f
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-07-23 00:41:45 +0530

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -2,9 +2,24 @@ public class Functions{
 // prepare a void function
   // prepare a function with parameters 
   // prepare a function using paramater and returntype
-  // 3 methods should contain same name
+  // 3 methods should contain same name 
+  public void name(String name){
+          System.out.println("function is created");
+          }
+    public void name1(String name){
+          System.out.println("shashi"+"preetham");
+          }
+       public String name2(String name){
+            System.out.println("shashi");
+            return name;
+          }
+
   public static void main(String[]args){
   Functions f=new Functions();
   // Call three Functions here
+          f.name("shashi");
+          f.name1("shashi");
+           f.name2("shashi");
+            
   }
 }
