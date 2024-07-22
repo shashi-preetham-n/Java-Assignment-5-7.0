20b7b7569d0641f97dd198728031fbb1c8ce635c
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-07-23 00:26:29 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,3 +1,17 @@
 public class Loops{
 // Prepare a For Loop and While  in each Function
+    public static void main(String[] args) {
+    int a=10;
+    for( int i=0;i<=a;i++){
+        System.out.println(a);
+    }
+    int count=0;
+     while (count<10) {
+        System.out.println(count);
+        count++;
+        
+     }
+
+}
+
 }
