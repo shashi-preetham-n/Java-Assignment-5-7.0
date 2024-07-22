271de97e4fba3929d4a7671fff255689a8e64ad9
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-07-23 00:39:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
 // Scanner Implementations
+ public static void main(String[] args) throws Exception {
+           Scanner sc= new Scanner(System.in);
+          int a=sc.nextInt();
+           int b=sc.nextInt();
+        
+           if (a>b){
+             System.out.println("a is greater than b");
+           }
+           else{
+             System.out.println("b is greater than a");
+           }
+          }
+}
