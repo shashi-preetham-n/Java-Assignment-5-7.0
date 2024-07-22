3c4f2fb4432e866f252ae4993b2954e9d5463dae
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-07-23 00:14:45 +0530

    Update IfElse.java

--- IfElse.java
+++ IfElse.java
@@ -1,4 +1,18 @@
 public class IfElse{
 //Take a variable and scan value using Scanner
   // Make Conditions using if else
+  
+          int a = 10;
+          if (a<b) {
+            System.out.println("a is greater than b");
+          }
+          else {
+            System.out.println("b is greater than a");
+          }
+          public static void main(String[] args) {
+           Scanner sc= new Scanner(System.in);
+          int a=sc.nextInt();
+           int b=sc.nextInt();
+          }
+
 }
