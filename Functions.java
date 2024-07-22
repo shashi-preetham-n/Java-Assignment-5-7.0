5729a51ca6de9e9188e192cafacd543718ca519b
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-22 12:05:08 +0530

    Create Functions.java

---
+++ Functions.java
@@ -0,0 +1,10 @@
+public class Functions{
+// prepare a void function
+  // prepare a function with parameters 
+  // prepare a function using paramater and returntype
+  // 3 methods should contain same name
+  public static void main(String[]args){
+  Functions f=new Functions();
+  // Call three Functions here
+  }
+}
